b7c8a2bac311eb77b75936833c6ebb72ca79c0e6
Rhaguer <ricardo.haguer@inacapmail.cl>
2024-09-10 19:45:27 -0300

    actualizar

--- TIPOPRUEBA/core/urls.py
+++ TIPOPRUEBA/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),  
+    path('', Login, name='Login'),  
+    path('index/', index, name='index'),
     path('contacto/', contacto, name='contacto'),
-    path('Nosotros/', nosotros, name='Nosotros'),
 ]
--- TIPOPRUEBA/core/views.py
+++ TIPOPRUEBA/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
+def Login(request):
+    return render(request, 'core/Login.html')
+
 def index(request):
     return render(request, 'core/index.html')
 
 def contacto(request):
     return render(request, 'core/contacto.html')
-
-def nosotros(request):
-    return render(request, 'core/Nosotros.html')
